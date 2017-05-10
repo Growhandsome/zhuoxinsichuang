024f6688962d4182ca2c598eb8ce3a984e9a5374
lilia <1311042097@qq.com>
2017-05-10 11:48:52 +0800

    改

--- js/index.js
+++ js/index.js
@@ -28,3 +28,15 @@ $(function(){
 	};
 
 });
+
+
+
+$(function(){
+	$('.header .nav .menu .list').hover(function(){
+		$(this).find('.submenu').slideDown('600');
+	},function(){
+		$(this).find('.submenu').slideUp('100');
+	});
+	
+	
+})
