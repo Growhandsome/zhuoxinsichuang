25fa1565f372bf2e16062446ecf267c574dc5c62
lilia <1311042097@qq.com>
2017-05-10 11:37:33 +0800

    frist commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+//banner
+$(function(){
+	var img = ['src/img/banner01_bg.jpg','src/img/banner02_bg.jpg','src/img/banner01_bg.jpg']
+	var index=0;
+	$('.banner .banner_btn .dot .left_arrow').click(function(){
+		index--;
+		if(index<0){
+			index=img.length-1;
+		}
+		active();
+	});
+	$('.banner .banner_btn .dot .right_arrow').click(function(){
+		index++;
+		if(index>img.length-1){
+			index=0;
+		}
+		active();
+	});
+	$('.banner .banner_btn .dot .center_arrow span').click(function(){
+		index = $(this).index();
+		active();
+	});
+	
+	function active(){
+		$('.banner .banner_btn .dot span').css({background:'#fff'});
+		$('.banner .banner_btn .dot span').eq(index).css({background:'#336089'});
+		$('.banner img').attr('src',img[index]);
+	};
+
+});
