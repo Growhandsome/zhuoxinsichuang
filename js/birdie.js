0d6af41bf2a1288d98a5f781843a24f95c55c7a0
lilia <1311042097@qq.com>
2017-05-17 18:00:08 +0800

    5.17xiugai

---
+++ js/birdie.js
@@ -0,0 +1,15 @@
+// $(function(){
+//     $('.top_menu').animate({
+//         position: fixed,
+//         top:0
+//     })
+
+
+
+// });
+
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+        anchors: ['firstPage','secondPage','thirdPage','lastPage']
+    });
+});
--- js/dynamic.js
+++ js/dynamic.js
@@ -4,6 +4,20 @@ $(function(){
 	},function(){
 		$(this).find('.submenu').slideUp('100');
 	});
-	
-	
+});
+
+
+$(function(){
+	$(document).scroll(function(){
+		if($(this).scrollTop() > 500){
+			$('.floating_sign').fadeIn();
+		}else{
+			$('.floating_sign').fadeOut();
+		};	
+	});
+	$('.floating_sign .top_arrow').click(function(){
+		$(document.body).animate({
+			scrollTop:0
+		})
+	});
 });
